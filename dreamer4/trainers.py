e2805925102281d7e99acdd78cb2292a1da71bbc
lucidrains <lucidrains@gmail.com>
2025-10-21 10:54:47 -0700

    tweak bc trainer

--- dreamer4/trainers.py
+++ dreamer4/trainers.py
@@ -195,7 +195,14 @@ class BehaviorCloneTrainer(Module):
         for _ in range(self.num_train_steps):
             batch_data = next(iter_train_dl)
 
-            loss = self.model(**batch_data)
+            # just assume raw video dynamics training if batch_data is a tensor
+            # else kwargs for video, actions, rewards
+
+            if is_tensor(batch_data):
+                loss = self.model(batch_data)
+            else:
+                loss = self.model(**batch_data)
+
             self.accelerator.backward(loss)
 
             if exists(self.max_grad_norm):
